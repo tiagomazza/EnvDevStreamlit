db519eb4e837d3b91e9c4d2a72c76f96332fec03
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-11-11 15:47:19 +0000

    else: insira a senha correta.

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ try:
         st.write(f"[Aceder a planilha](https://docs.google.com/spreadsheets/d/1ujI1CUkvZoAYuucX4yrV2Z5BN3Z8-o-Kqm3PAfMqi0I/edit?gid=1541275584#gid=1541275584)")
         st.write(f"[Aceder a documentação](https://docs.google.com/document/d/1wgndUW2Xb48CBi6BSgSBRVw2sdqgqFtZxg_9Go5GYLg/edit?usp=sharing)")
 
-    elif pagina_selecionada == "🔐Restrito":
+    elif pagina_selecionada == "🔐Restrito" and entered_password == senha_admin:
       
         st.title("🔐Restrito")
 
@@ -326,7 +326,10 @@ try:
         st.write(f"[Aceder a planilha](https://docs.google.com/spreadsheets/d/1ujI1CUkvZoAYuucX4yrV2Z5BN3Z8-o-Kqm3PAfMqi0I/edit?gid=1541275584#gid=1541275584)")
         st.write(f"[Aceder a documentação](https://docs.google.com/document/d/1wgndUW2Xb48CBi6BSgSBRVw2sdqgqFtZxg_9Go5GYLg/edit?usp=sharing)")
 
-        
+    else:
+        if pagina_selecionada in ["🔍Consultas", "🔐Restrito"]:
+            st.warning("Acesso restrito. Insira a senha correta.")   
+               
 except ValueError:
     print("Invalid password format. Please enter a valid integer.")
     pass
